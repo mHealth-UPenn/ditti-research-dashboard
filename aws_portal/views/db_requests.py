6e773975ed7c091113fe74e88eb4d1dd2531660c
Jesse Stover <j.stover.r@gmail.com>
2022-01-19 14:27:15 -0500

    study details/study contacts/account details

--- aws_portal/views/db_requests.py
+++ aws_portal/views/db_requests.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, json, jsonify
 
 blueprint = Blueprint('db', __name__, url_prefix='/db')
 
@@ -10,4 +10,19 @@ def apps():
 
 @blueprint.route('/studies')
 def studies():
+    return jsonify({})
+
+
+@blueprint.route('/study-details')
+def study_details():
+    return jsonify({})
+
+
+@blueprint.route('/study-contacts')
+def study_contacts():
+    return jsonify({})
+
+
+@blueprint.route('/account-details')
+def account_details():
     return jsonify({})
