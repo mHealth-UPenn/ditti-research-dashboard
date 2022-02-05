7e3384ed973e9f29fed48f4739d979e238fe0a98
Jesse Stover <j.stover.r@gmail.com>
2022-02-05 14:53:23 -0500

    check for relationships

--- aws_portal/utils/db.py
+++ aws_portal/utils/db.py
@@ -1,9 +1,15 @@
+from aws_portal.extensions import db
+
+
 def populate_model(model, data):
     for k, v in data.items():
         if not hasattr(model, k):
             raise ValueError('Invalid attribute: %s' % k)
 
         if isinstance(v, list):
+            continue
+
+        if isinstance(getattr(model, k), db.Model):
             continue
 
         setattr(model, k, v)
