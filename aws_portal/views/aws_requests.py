724f0bb50eed5b840b9c0b0f2e30750edfcaa762
Jesse Stover <j.stover.r@gmail.com>
2022-03-09 12:44:34 -0500

    test_user_edit

--- aws_portal/views/aws_requests.py
+++ aws_portal/views/aws_requests.py
@@ -1,5 +1,6 @@
 import re
 from flask import Blueprint, jsonify, request
+from aws_portal.models import Study
 from aws_portal.utils.auth import auth_required
 from aws_portal.utils.aws import Query
 
@@ -28,6 +29,25 @@ def scan():
 @blueprint.route('/user/create')
 @auth_required('Create', 'User')
 def user_create():
+    app = request.json.get('app')
+    user_permission_id = request.args.get('user_permission_id')
+
+    acronym = re.sub(r'[\d]+', '', user_permission_id)
+    study_id = request.json.get('study')
+    study = Study.query.get(study_id)
+
+    if acronym != study.ditti_id:
+        return jsonify({'msg': 'Invalid study acronym: %s' % acronym})
+
+    if re.search(r'[^\dA-Za-z]', user_permission_id) is not None:
+        return jsonify({'msg': 'Invalid Ditti ID: %s' % user_permission_id})
+
+    query = 'user_permission_id==' % user_permission_id
+    res = Query(app, 'User', query).scan()
+
+    if not res['Items']:
+        return jsonify({'msg': 'Ditti ID not found: %s' % user_permission_id})
+
     return jsonify({})
 
 
--- tests/tests_views/test_aws_requests.py
+++ tests/tests_views/test_aws_requests.py
@@ -1,10 +1,16 @@
+from functools import partial
 from flask import json
 import pytest
 from aws_portal.app import create_app
+from aws_portal.extensions import db
 from aws_portal.models import (
     init_admin_account, init_admin_app, init_admin_group, init_db
 )
-from tests.testing_utils import login_admin_account
+from aws_portal.utils.aws import Query
+from tests.testing_utils import (
+    create_joins, create_tables, get_csrf_headers, login_admin_account,
+    login_test_account
+)
 
 
 @pytest.fixture
@@ -15,6 +21,9 @@ def app():
         init_admin_app()
         init_admin_group()
         init_admin_account()
+        create_tables()
+        create_joins()
+        db.session.commit()
         yield app
 
 
@@ -22,6 +31,19 @@ def app():
 def client(app):
     with app.test_client() as client:
         yield client
+
+
+@pytest.fixture
+def post(client):
+    res = login_test_account('foo', client)
+    headers = get_csrf_headers(res)
+    post = partial(
+        client.post,
+        content_type='application/json',
+        headers=headers
+    )
+
+    yield post
 
 
 def test_scan_invalid(client):
@@ -54,8 +76,44 @@ def test_user_create(client):
     raise NotImplementedError
 
 
-def test_user_edit(client):
-    raise NotImplementedError
+def test_user_edit(post):
+    query = 'user_permission_id==foo000'
+    data = {
+        'group': 2,
+        'study': 1,
+        'app': 'DittiApp',
+        'user_permission_id': 'FOO000',
+        'edit': {
+            'team_email': 'bar@email.com'
+        }
+    }
+
+    res = Query('DittiApp', 'User', query).scan()
+    assert len(res['Items']) == 1
+    assert 'team_email' in res['Items'][0]
+    assert res['Items'][0]['team_email'] == 'foo@email.com'
+
+    data = json.dumps(data)
+    res = post('/aws/user/edit', data=data)
+    res = json.loads(res)
+    assert 'msg' in res
+    assert res['msg'] == 'User Successfully Edited'
+
+    res = Query('DittiApp', 'User', query).scan()
+    assert len(res['Items']) == 1
+    assert 'team_email' in res['Items'][0]
+    assert res['Items'][0]['team_email'] == 'bar@email.com'
+
+    data = json.dumps(data)
+    res = post('/aws/user/edit', data=data)
+    res = json.loads(res)
+    assert 'msg' in res
+    assert res['msg'] == 'User Successfully Edited'
+
+    res = Query('DittiApp', 'User', query).scan()
+    assert len(res['Items']) == 1
+    assert 'team_email' in res['Items'][0]
+    assert res['Items'][0]['team_email'] == 'foo@email.com'
 
 
 def test_user_archive(client):
