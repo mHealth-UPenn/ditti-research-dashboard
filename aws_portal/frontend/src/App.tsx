038f825159df93072d51e5247c10d0cd5217262c
Jesse Stover <j.stover.r@gmail.com>
2022-04-01 12:46:33 -0400

    studiesMenu

--- aws_portal/frontend/src/App.tsx
+++ aws_portal/frontend/src/App.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header";
+import StudiesMenu from "./components/studiesMenu";
 
 function App() {
+  const studies = [
+    { name: "MSBI", id: 1 },
+    { name: "ART OSA", id: 2 }
+  ];
+
   return (
     <main>
       <Header name="John Smith" email="john.smith@pennmedicine.upenn.edu" />
+      <StudiesMenu studies={studies} />
     </main>
   );
 }
--- aws_portal/frontend/src/components/studiesMenu.tsx
+++ aws_portal/frontend/src/components/studiesMenu.tsx
@@ -1,2 +1,32 @@
 import * as React from "react";
 import { Component } from "react";
+import "./studiesMenu.css";
+
+interface StudiesMenuProps {
+  studies: { name: string; id: number }[];
+}
+
+class StudiesMenu extends React.Component<StudiesMenuProps, any> {
+  render() {
+    const { studies } = this.props;
+
+    return (
+      <div className="bg-white studies-menu-container">
+        <div className="studies-menu-header">
+          <span>Studies</span>
+        </div>
+        <div className="studies-menu-content">
+          <ul>
+            {studies.map((s) => (
+              <li key={s.id} id={"study-menu-" + s.id}>
+                {s.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StudiesMenu;
