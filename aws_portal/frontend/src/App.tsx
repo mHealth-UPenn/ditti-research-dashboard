509efa3f067c297587c9d0af8316b65be2a5ad7b
Jesse Stover <j.stover.r@gmail.com>
2022-04-04 10:57:44 -0400

    wrap all components in the dashboard

--- aws_portal/frontend/src/App.tsx
+++ aws_portal/frontend/src/App.tsx
@@ -1,35 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
-import AppView from "./components/views/appView";
 import Dashboard from "./components/dashboard";
-import Header from "./components/header";
-import HomeView from "./components/views/homeView";
-import Navbar from "./components/navbar";
-import StudiesMenu from "./components/studiesMenu";
 
 function App() {
-  const studies = [
-    { name: "MSBI", id: 1 },
-    { name: "ART OSA", id: 2 }
-  ];
-
-  const breadcrumbs = [
-    { name: "Home", view: () => HomeView },
-    { name: "Ditti App", view: () => AppView }
-  ];
-
-  return (
-    <Dashboard>
-      <Header name="John Smith" email="john.smith@pennmedicine.upenn.edu" />
-      <div style={{ display: "flex", flexGrow: 1 }}>
-        <StudiesMenu studies={studies} />
-        <div style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
-          <Navbar breadcrumbs={breadcrumbs} />
-        </div>
-      </div>
-    </Dashboard>
-  );
+  return <Dashboard />;
 }
 
 export default App;
--- aws_portal/frontend/src/components/dashboard.tsx
+++ aws_portal/frontend/src/components/dashboard.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 import { Component } from "react";
+import AppView from "./views/appView";
+import Header from "./header";
+import HomeView from "./views/homeView";
+import Navbar from "./navbar";
+import StudiesMenu from "./studiesMenu";
 import "./dashboard.css";
 
 // interface DashboardProps {}
@@ -9,8 +14,28 @@ import "./dashboard.css";
 class Dashboard extends React.Component<any, any> {
   // state = { :  }
   render() {
+    const studies = [
+      { name: "MSBI", id: 1 },
+      { name: "ART OSA", id: 2 }
+    ];
+
+    const breadcrumbs = [
+      { name: "Home", view: () => HomeView },
+      { name: "Ditti App", view: () => AppView }
+    ];
+
     return (
-      <div className="bg-light dashboard-container">{this.props.children}</div>
+      <main className="bg-light dashboard-container">
+        <Header name="John Smith" email="john.smith@pennmedicine.upenn.edu" />
+        <div style={{ display: "flex", flexGrow: 1 }}>
+          <StudiesMenu studies={studies} />
+          <div
+            style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}
+          >
+            <Navbar breadcrumbs={breadcrumbs} />
+          </div>
+        </div>
+      </main>
     );
   }
 }
