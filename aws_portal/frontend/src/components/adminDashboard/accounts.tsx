c856f3753df6652096b68d719b7ff32199ce67ca
Jesse Stover <j.stover.r@gmail.com>
2022-04-18 10:46:28 -0400

    admin dashboard navbar

---
+++ aws_portal/frontend/src/components/adminDashboard/accounts.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Component } from "react";
+
+// interface AccountsProps {}
+
+// interface AccountsState {}
+
+class Accounts extends React.Component<any, any> {
+  // state = { :  }
+  render() {
+    return <div>Accounts!</div>;
+  }
+}
+
+export default Accounts;
--- aws_portal/frontend/src/components/adminDashboard/adminDashboard.tsx
+++ aws_portal/frontend/src/components/adminDashboard/adminDashboard.tsx
@@ -1,17 +1,46 @@
 import * as React from "react";
 import { Component } from "react";
+import Accounts from "./accounts";
 import "./adminDashboard.css";
 
 // interface AdminDashboardProps {}
 
-// interface AdminDashboardState {}
+interface AdminDashboardState {
+  views: { active: boolean; name: string; view: React.ReactElement }[];
+}
 
-class AdminDashboardView extends React.Component<any, any> {
-  // state = { :  }
+class AdminDashboardView extends React.Component<any, AdminDashboardState> {
+  state = {
+    views: [
+      { active: true, name: "Accounts", view: <Accounts /> },
+      { active: false, name: "Studies", view: <Accounts /> },
+      { active: false, name: "Access Groups", view: <Accounts /> },
+      { active: false, name: "Apps", view: <Accounts /> }
+    ]
+  };
+
+  setView = (view: React.ReactElement) => {
+    console.log(view);
+  };
+
   render() {
+    const { views } = this.state;
+
     return (
       <div className="page-container">
-        <div className="page-header bg-white border-dark-b">AdminDashboard</div>
+        <div className="page-header bg-white border-dark-b">
+          {views.map((v) => (
+            <div
+              className={
+                "page-header-button" +
+                (v.active ? " bg-dark" : " link-no-underline")
+              }
+              onClick={() => this.setView(v.view)}
+            >
+              {v.name}
+            </div>
+          ))}
+        </div>
         <div className="page-content bg-white">Content!</div>
       </div>
     );
