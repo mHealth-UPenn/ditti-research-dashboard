dc2c9c00eb3251ca4e87689deb366cffd99b46a8
Jesse Stover <j.stover.r@gmail.com>
2022-04-18 13:01:04 -0400

    tableControl

--- aws_portal/frontend/src/components/table/tableControl.tsx
+++ aws_portal/frontend/src/components/table/tableControl.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Component } from "react";
+import TextField from "../fields/textField";
 
 interface TableControlProps {
   control: React.ReactElement;
@@ -13,7 +14,29 @@ interface TableControlProps {
 
 class TableControl extends React.Component<TableControlProps, any> {
   render() {
-    return <div>TableControl</div>;
+    const { control, controlWidth, includeControl, includeSearch, onSearch } =
+      this.props;
+
+    return (
+      <div className="table-control-container">
+        includeSearch ?
+        <div className="table-search">
+          <TextField
+            id="table-search-input"
+            svg={<React.Fragment />}
+            type="text"
+            placeholder="Search..."
+            prefill=""
+            label=""
+            feedback=""
+          />
+        </div>
+        includeControl ?
+        <div className="table-control" style={{ width: controlWidth }}>
+          {control}
+        </div>
+      </div>
+    );
   }
 }
 
