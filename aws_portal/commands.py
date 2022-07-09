8b6d310be509f4af17d5a36606923329141eee29
Jesse Stover <j.stover.r@gmail.com>
2022-07-09 12:12:09 -0400

    added init command overrids

--- aws_portal/commands.py
+++ aws_portal/commands.py
@@ -1,4 +1,5 @@
 import click
+from flask import current_app
 from flask.cli import with_appcontext
 from aws_portal.models import (
     init_admin_app, init_admin_group, init_admin_account, init_db
@@ -6,22 +7,34 @@ from aws_portal.models import (
 
 
 @click.command('init-admin-app')
+@click.option('--uri', default=None, help='Overrides the SQLAlchemy URI.')
 @with_appcontext
-def init_admin_app_click():
+def init_admin_app_click(uri):
+    if uri is not None:
+        current_app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
     app = init_admin_app()
     click.echo(repr(app))
 
 
 @click.command('init-admin-group')
+@click.option('--uri', default=None, help='Overrides the SQLAlchemy URI.')
 @with_appcontext
-def init_admin_group_click():
+def init_admin_group_click(uri):
+    if uri is not None:
+        current_app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
     access_group = init_admin_group()
     click.echo(repr(access_group))
 
 
 @click.command('init-admin')
+@click.option('--uri', default=None, help='Overrides the SQLAlchemy URI.')
 @with_appcontext
-def init_admin_account_click():
+def init_admin_account_click(uri):
+    if uri is not None:
+        current_app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
     admin = init_admin_account()
     click.echo(repr(admin))
 
