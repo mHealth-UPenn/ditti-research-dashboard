f00a17bc882bc486f1186457b1922f6167fbb2cf
Jesse Stover <j.stover.r@gmail.com>
2024-09-24 11:01:38 -0400

    Responsive studies menu

--- aws_portal/frontend/src/components/studiesMenu.tsx
+++ aws_portal/frontend/src/components/studiesMenu.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import { Study, TapDetails, ViewProps } from "../interfaces";
 import { makeRequest } from "../utils";
 import StudySummary from "./dittiApp/studySummary";
 import { SmallLoader } from "./loader";
 import "./studiesMenu.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 /**
  * getTaps: get tap data
@@ -22,7 +25,9 @@ const StudiesMenu: React.FC<StudiesMenuProps> = ({
   setView,
 }) => {
   const [studies, setStudies] = useState<Study[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState(true);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const drawerRef = createRef<HTMLDivElement>();
 
   useEffect(() => {
     // get the studies that the user has access to
@@ -32,40 +37,63 @@ const StudiesMenu: React.FC<StudiesMenuProps> = ({
     });
   }, []);
 
+  const handleClickMenu = () => {
+    if (drawerRef.current) {
+      drawerRef.current.style.width = "16rem";
+      drawerRef.current.style.padding = "2rem";
+      setDrawerOpen(true);
+    }
+  };
+
+  const handleClickClose = () => {
+    if (drawerRef.current) {
+      drawerRef.current.style.width = "0";
+      drawerRef.current.style.padding = "0";
+      setDrawerOpen(false);
+    }
+  };
+
+  const studiesList = (
+    <ul className="m-0 p-0 list-none">
+      {/* render a list of studies */}
+      {studies.map((s: Study, i: number) => (
+        <li
+          key={i}
+          className="mb-4 text-[#666699] cursor-pointer underline"
+          id={"study-menu-" + s.id}
+          onClick={() =>
+            setView(
+              s.acronym,
+              <StudySummary
+                flashMessage={flashMessage}
+                handleClick={handleClick}
+                getTaps={getTaps}
+                goBack={goBack}
+                studyId={s.id}
+              />
+            )
+          }
+        >
+          {s.acronym}
+        </li>
+      ))}
+    </ul>
+  );
+
   return (
-    <div className="bg-white flex flex-col flex-shrink-0 w-64 border-r border-solid border-[#33334D]">
-      <div className="flex items-center font-bold h-16 pl-8 border-b border-solid border-[#33334D]">
-        <span>Studies</span>
+    <div className="bg-white flex flex-col flex-shrink-0 border-r border-solid border-[#33334D] w-16 xl:w-64">
+      <div className="flex items-center font-bold h-16 xl:pl-8 border-b border-solid border-[#33334D]">
+        <div className="flex flex-grow items-center justify-center h-full cursor-pointer xl:hidden" onClick={drawerOpen ? handleClickClose : handleClickMenu}>
+          {drawerOpen ? <CloseIcon /> : <MenuIcon />}
+        </div>
+        <span className="hidden xl:block">Studies</span>
       </div>
-      <div className="p-8">
-        {loading ? (
-          <SmallLoader />
-        ) : (
-          <ul className="m-0 p-0 list-none">
-            {/* render a list of studies */}
-            {studies.map((s: Study, i: number) => (
-              <li
-                key={i}
-                className="mb-4 text-[#666699] cursor-pointer underline"
-                id={"study-menu-" + s.id}
-                onClick={() =>
-                  setView(
-                    s.acronym,
-                    <StudySummary
-                      flashMessage={flashMessage}
-                      handleClick={handleClick}
-                      getTaps={getTaps}
-                      goBack={goBack}
-                      studyId={s.id}
-                    />
-                  )
-                }
-              >
-                {s.acronym}
-              </li>
-            ))}
-          </ul>
-        )}
+      <div className="p-8 hidden xl:flex">
+        {loading ? <SmallLoader /> : studiesList}
+      </div>
+      <div ref={drawerRef} className="w-0 overflow-hidden p-0 flex flex-col w-64 bg-white z-10 flex-grow border-r border-solid border-[#33334D]">
+        <span className="mb-4 font-bold">Studies</span>
+        {loading ? <SmallLoader /> : studiesList}
       </div>
     </div>
   );
