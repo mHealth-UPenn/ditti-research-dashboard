10a52dbbb147e8d737321d835d70216896ffac06
Jesse Stover <j.stover.r@gmail.com>
2024-10-30 17:13:49 -0500

    Pan left and right

--- aws_portal/frontend/src/components/dittiApp/TimestampHistogram.tsx
+++ aws_portal/frontend/src/components/dittiApp/TimestampHistogram.tsx
@@ -5,6 +5,7 @@ import { Brush } from '@visx/brush';
 import { AxisLeft, AxisBottom } from '@visx/axis';
 import { bin as d3Histogram } from 'd3-array';
 import { Bounds } from '@visx/brush/lib/types';
+import { Group } from '@visx/group';
 
 interface HistogramProps {
   timestamps: number[];
@@ -13,7 +14,7 @@ interface HistogramProps {
 
 const Histogram: React.FC<HistogramProps> = ({ timestamps }) => {
   const [zoomDomain, setZoomDomain] = useState<[Date, Date] | null>(null);
-  const width = 800;
+  const width = 600;
   const height = 400;
   const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
@@ -27,14 +28,12 @@ const Histogram: React.FC<HistogramProps> = ({ timestamps }) => {
       domain: zoomDomain ?? [previousNoon, todayNoon],
       range: [margin.left, width - margin.right],
     });
-    console.log(scale.domain());
     return scale;
   }, [zoomDomain]);
 
   const histogramData = useMemo(() => {
-    const binSize = calculateBinSize(width);
     const domain = xScale.domain();
-    const thresholds = xScale.ticks(binSize).map(t => t.getTime());
+    const thresholds = xScale.ticks(40).map(t => t.getTime());
     const bins = d3Histogram()
       .domain([domain[0].getTime(), domain[1].getTime()])
       .thresholds(thresholds)(timestamps);
@@ -44,7 +43,7 @@ const Histogram: React.FC<HistogramProps> = ({ timestamps }) => {
 
   const yScale = useMemo(() => {
     return scaleLinear({
-      domain: [0, Math.max(...histogramData.map((bin) => bin.length))],
+      domain: [0, Math.max(...histogramData.map((bin) => bin.length)) + 10],
       range: [height - margin.bottom, margin.top],
     });
   }, [histogramData]);
@@ -55,48 +54,78 @@ const Histogram: React.FC<HistogramProps> = ({ timestamps }) => {
 
   const resetZoom = () => setZoomDomain(null);
 
+  const panLeft = () => {
+    const [left, right] = zoomDomain ?? [previousNoon, todayNoon];
+    const leftTimestamp = left.getTime();
+    const rightTimestamp = right.getTime();
+    const panAmount = (rightTimestamp - leftTimestamp) / 2;
+    const newLeft = new Date(leftTimestamp - panAmount);
+    const newRight = new Date(rightTimestamp - panAmount);
+    setZoomDomain([newLeft, newRight]);
+  };
+
+  const panRight = () => {
+    const [left, right] = zoomDomain ?? [previousNoon, todayNoon];
+    const leftTimestamp = left.getTime();
+    const rightTimestamp = right.getTime();
+    const panAmount = (rightTimestamp - leftTimestamp) / 2;
+    const newLeft = new Date(leftTimestamp + panAmount);
+    const newRight = new Date(rightTimestamp + panAmount);
+    setZoomDomain([newLeft, newRight]);
+  };
+
   return (
-    <svg width={width} height={height}>
-      <rect width={width} height={height} fill="#f3f3f3" />
-      <AxisBottom top={height - margin.bottom} scale={xScale} />
-      <AxisLeft left={margin.left} scale={yScale} />
+    <React.Fragment>
+      <button
+        className="button button-lg button-primary font-bold"
+        onClick={resetZoom}>
+          Reset
+      </button>
+      <button
+        className="button button-lg button-primary font-bold"
+        onClick={panLeft}>
+          Left
+      </button>
+      <button
+        className="button button-lg button-primary font-bold"
+        onClick={panRight}>
+          Right
+      </button>
+      <svg width={width} height={height}>
+        <rect width={width} height={height} fill="white" />
 
-      {
-        histogramData.map((bin, index) => (
-          <Bar
-            key={`bar-${index}`}
-            x={xScale(bin.x0 ? bin.x0 : 0) ?? 0}
-            y={yScale(bin.length)}
-            height={yScale(0) - yScale(bin.length)}
-            width={Math.max(0, xScale(bin.x1 ? bin.x1 : 0) - xScale(bin.x0 ? bin.x0 : 0))}
-            fill="teal" />
-        ))
-      }
-
-      <Brush
-        xScale={xScale}
-        yScale={yScale}
-        width={width - margin.left - margin.right}
-        height={height - margin.top - margin.bottom}
-        onBrushEnd={(bounds: Bounds | null) => {
-          if (bounds) {
-            onZoomChange([bounds.x0, bounds.x1]);
+        <Group>
+          {
+            histogramData.map((bin, index) => (
+              <Bar
+                key={`bar-${index}`}
+                x={xScale(bin.x0 ? bin.x0 : 0) ?? 0}
+                y={yScale(bin.length)}
+                height={yScale(0) - yScale(bin.length)}
+                width={Math.max(0, xScale(bin.x1 ? bin.x1 : 0) - xScale(bin.x0 ? bin.x0 : 0))}
+                fill="#33334D"
+                stroke="white" />
+            ))
           }
-        }}
-        resetOnEnd={true} />
-      <foreignObject x={20} y={20} width={70} height={60}>
-        <button onClick={resetZoom}>Reset Zoom</button>
-      </foreignObject>
-    </svg>
+
+          <AxisBottom top={height - margin.bottom} scale={xScale} />
+          <AxisLeft left={margin.left} scale={yScale} />
+
+          <Brush
+            xScale={xScale}
+            yScale={yScale}
+            width={width - margin.left - margin.right}
+            height={height - margin.top - margin.bottom}
+            onBrushEnd={(bounds: Bounds | null) => {
+              if (bounds) {
+                onZoomChange([bounds.x0, bounds.x1]);
+              }
+            }}
+            resetOnEnd={true} />
+        </Group>
+      </svg>
+    </React.Fragment>
   );
 };
-
-// Utility function to calculate bin size based on window width
-function calculateBinSize(width: number) {
-  if (width > 1200) return 1; // 1-minute bin size
-  if (width > 800) return 5; // 5-minute bin size
-  if (width > 500) return 15; // 15-minute bin size
-  return 60; // hourly bin size
-}
 
 export default Histogram;
