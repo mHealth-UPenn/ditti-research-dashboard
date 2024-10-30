791eb081f1eb074025613de7368693a5b6210c9b
Jesse Stover <j.stover.r@gmail.com>
2024-10-30 17:33:07 -0500

    Zoom functionality

--- aws_portal/frontend/src/components/dittiApp/TimestampHistogram.tsx
+++ aws_portal/frontend/src/components/dittiApp/TimestampHistogram.tsx
@@ -14,6 +14,9 @@ interface HistogramProps {
 
 const Histogram: React.FC<HistogramProps> = ({ timestamps }) => {
   const [zoomDomain, setZoomDomain] = useState<[Date, Date] | null>(null);
+  const [minRangeReached, setMinRangeReached] = useState(false);
+  const [maxRangeReached, setMaxRangeReached] = useState(false);
+
   const width = 600;
   const height = 400;
   const margin = { top: 20, right: 30, bottom: 30, left: 40 };
@@ -49,6 +52,18 @@ const Histogram: React.FC<HistogramProps> = ({ timestamps }) => {
   }, [histogramData]);
 
   const onZoomChange = (domain: [number, number]) => {
+    const [left, right] = domain;
+    let range = right - left;
+
+    // If the resulting range is less than or equal to 30 minutes
+    if (range <= 1800000) {
+      range = 1800000;
+      setMinRangeReached(true);
+    } else {
+      setMinRangeReached(false);
+    }
+    setMaxRangeReached(false);
+
     setZoomDomain([new Date(domain[0]), new Date(domain[1])]);
   };
 
@@ -74,6 +89,46 @@ const Histogram: React.FC<HistogramProps> = ({ timestamps }) => {
     setZoomDomain([newLeft, newRight]);
   };
 
+  const zoomIn = () => {
+    const [left, right] = zoomDomain ?? [previousNoon, todayNoon];
+    const leftTimestamp = left.getTime();
+    const rightTimestamp = right.getTime();
+    let range = rightTimestamp - leftTimestamp;
+
+    // If the resulting range is less than or equal to 30 minutes
+    if (range <= 1800000) {
+      range = 1800000;
+      setMinRangeReached(true);
+    } else {
+      setMinRangeReached(false);
+    }
+    setMaxRangeReached(false);
+
+    const newLeft = new Date(leftTimestamp + range / 4);
+    const newRight = new Date(rightTimestamp - range / 4);
+    setZoomDomain([newLeft, newRight]);
+  };
+
+  const zoomOut = () => {
+    const [left, right] = zoomDomain ?? [previousNoon, todayNoon];
+    const leftTimestamp = left.getTime();
+    const rightTimestamp = right.getTime();
+    let range = rightTimestamp - leftTimestamp;
+
+    // If the resulting range is greater than or equal to 7 days
+    if (range >= 518400000) {
+      range = 1800000;
+      setMaxRangeReached(true);
+    } else {
+      setMaxRangeReached(false);
+    }
+    setMinRangeReached(false);
+
+    const newLeft = new Date(leftTimestamp - range / 2);
+    const newRight = new Date(rightTimestamp + range / 2);
+    setZoomDomain([newLeft, newRight]);
+  };
+
   return (
     <React.Fragment>
       <button
@@ -91,8 +146,33 @@ const Histogram: React.FC<HistogramProps> = ({ timestamps }) => {
         onClick={panRight}>
           Right
       </button>
+      <button
+        className="button button-lg button-primary font-bold"
+        onClick={zoomIn}
+        disabled={minRangeReached}>
+          Zoom In
+      </button>
+      <button
+        className="button button-lg button-primary font-bold"
+        onClick={zoomOut}
+        disabled={maxRangeReached}>
+          Zoom Out
+      </button>
+
       <svg width={width} height={height}>
         <rect width={width} height={height} fill="white" />
+
+        <Brush
+          xScale={xScale}
+          yScale={yScale}
+          width={width - margin.left - margin.right}
+          height={height - margin.bottom}
+          onBrushEnd={(bounds: Bounds | null) => {
+            if (bounds) {
+              onZoomChange([bounds.x0, bounds.x1]);
+            }
+          }}
+          resetOnEnd={true} />
 
         <Group>
           {
@@ -102,26 +182,13 @@ const Histogram: React.FC<HistogramProps> = ({ timestamps }) => {
                 x={xScale(bin.x0 ? bin.x0 : 0) ?? 0}
                 y={yScale(bin.length)}
                 height={yScale(0) - yScale(bin.length)}
-                width={Math.max(0, xScale(bin.x1 ? bin.x1 : 0) - xScale(bin.x0 ? bin.x0 : 0))}
-                fill="#33334D"
-                stroke="white" />
+                width={Math.max(0, xScale(bin.x1 ? bin.x1 : 0) - xScale(bin.x0 ? bin.x0 : 0) - 1)}
+                fill="#33334D" />
             ))
           }
 
           <AxisBottom top={height - margin.bottom} scale={xScale} />
           <AxisLeft left={margin.left} scale={yScale} />
-
-          <Brush
-            xScale={xScale}
-            yScale={yScale}
-            width={width - margin.left - margin.right}
-            height={height - margin.top - margin.bottom}
-            onBrushEnd={(bounds: Bounds | null) => {
-              if (bounds) {
-                onZoomChange([bounds.x0, bounds.x1]);
-              }
-            }}
-            resetOnEnd={true} />
         </Group>
       </svg>
     </React.Fragment>
