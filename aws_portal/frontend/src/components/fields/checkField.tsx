494689322ee4fa851837039e7246d59f2a880615
Jesse Stover <j.stover.r@gmail.com>
2024-11-02 09:53:01 -0500

    Fixed responsive styling on checkbox field

--- aws_portal/frontend/src/components/fields/checkField.tsx
+++ aws_portal/frontend/src/components/fields/checkField.tsx
@@ -16,21 +16,25 @@ interface CheckFieldProps {
 
 const CheckField: React.FC<CheckFieldProps> = ({ id, prefill, label, onChange }) => {
   return (
-    <div>
-      {label ? (
-        <label className="mr-4" htmlFor={id}>
-          {label}
-        </label>
-      ) : null}
-      <input
-        type="checkbox"
-        checked={prefill}
-        onChange={
-          onChange
-            ? (e) => onChange((e.target as HTMLInputElement).checked)
-            : () => null
-        }
-      />
+    <div className="flex flex-col h-full">
+      <div className="hidden md:flex mb-1">&nbsp;</div>
+      <div className="flex flex-grow items-center">
+        <div>
+          {label &&
+            <label className="mr-4" htmlFor={id}>
+              {label}
+            </label>
+          }
+          <input
+            type="checkbox"
+            checked={prefill}
+            onChange={
+              onChange
+                ? (e) => onChange((e.target as HTMLInputElement).checked)
+                : () => null
+            } />
+        </div>
+      </div>
     </div>
   );
 };
