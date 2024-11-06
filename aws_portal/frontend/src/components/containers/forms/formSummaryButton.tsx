73acd9c6d61723a48834541e9908ad699b8911d8
Jesse Stover <j.stover.r@gmail.com>
2024-11-06 11:14:23 -0600

    Form summary styling

--- aws_portal/frontend/src/components/containers/forms/formSummaryButton.tsx
+++ aws_portal/frontend/src/components/containers/forms/formSummaryButton.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 
 const FormSummaryButton = ({ children }: PropsWithChildren<unknown>) => {
   return (
-    <div className="flex flex-col lg:w-full justify-end">
+    <div className="flex flex-col w-full md:w-1/2 lg:w-full justify-end">
       {children}
     </div>
   );
