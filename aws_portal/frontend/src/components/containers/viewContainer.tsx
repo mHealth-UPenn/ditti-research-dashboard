48b343dbc5348b923f405312e8cc3e9a5f2a4099
Jesse Stover <j.stover.r@gmail.com>
2024-11-16 11:34:56 -0600

    Participant dashboard info

--- aws_portal/frontend/src/components/containers/viewContainer.tsx
+++ aws_portal/frontend/src/components/containers/viewContainer.tsx
@@ -1,9 +1,21 @@
 import { PropsWithChildren } from "react";
 
+interface IViewContainerProps {
+  navbar?: boolean;
+}
 
-const ViewContainer = ({ children }: PropsWithChildren<unknown>) => {
+
+const ViewContainer = ({
+  navbar = true,
+  children
+}: PropsWithChildren<IViewContainerProps>) => {
+  const height =
+    navbar ?
+    "min-h-[calc(calc(100vh-8rem)-1px)] h-[calc(calc(100vh-8rem)-1px)]" :
+    "min-h-[calc(calc(100vh-4rem)-1px)] h-[calc(calc(100vh-4rem)-1px)]";
+
   return (
-    <div className="bg-extra-light min-h-[calc(calc(100vh-8rem)-1px)] h-[calc(calc(100vh-8rem)-1px)] px-4 py-16 md:px-6 overflow-scroll">
+    <div className={`bg-extra-light ${height} px-4 py-16 md:px-6 overflow-scroll`}>
       <div className="flex flex-wrap basis-[content]">
         {children}
       </div>
--- aws_portal/frontend/src/components/participantDashboard/participantDashboard.tsx
+++ aws_portal/frontend/src/components/participantDashboard/participantDashboard.tsx
@@ -5,6 +5,7 @@ import Card from '../cards/card';
 import CardContentRow from '../cards/cardContentRow';
 import Title from '../text/title';
 import Button from '../buttons/button';
+import Link from '../links/link';
 
 
 const ParticipantDashboard: React.FC = () => {
@@ -17,8 +18,38 @@ const ParticipantDashboard: React.FC = () => {
 
   const { cognitoLogout, dittiId } = useAuth();
 
+  useEffect(() => {
+    const init = async () => {
+      // TODO: pull data here
+      const start = new Date();
+      const end = new Date();
+      start.setDate(1);
+      end.setDate(28);
+
+      setFitbitConnected(true);
+      setStartDate(start);
+      setEndDate(end);
+      setScope(["Sleep"]);
+      setDataSummary(`
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget nisi
+        elit. Donec molestie, mauris sed sollicitudin convallis, mauris odio
+        convallis dolor, sit amet rhoncus dolor felis vel dui. Nam scelerisque
+        arcu nisi, ut mattis nibh suscipit sit amet. Ut lacinia facilisis
+        vulputate. Sed vitae enim turpis. Sed in molestie lacus, id tincidunt
+        enim. In posuere tempus egestas.
+      `);
+      setLoading(false);
+    };
+
+    init();
+  }, []);
+
   const handleRedirect = () => {
     window.location.href = 'http://localhost:5000/cognito/fitbit/authorize';
+  };
+
+  const handleClickManageData = () => {
+    window.location.href = "http://localhost:3000/";
   };
 
   return (
@@ -34,17 +65,56 @@ const ParticipantDashboard: React.FC = () => {
         </div>
       </div>
 
-      <ViewContainer>
+      <ViewContainer navbar={false}>
         <Card width="md">
           <CardContentRow>
             <Title>Your User ID: {dittiId}</Title>
-            <Button onClick={handleRedirect} rounded={true}>Connect FitBit</Button>
+            {!fitbitConnected &&
+              <Button onClick={handleRedirect} rounded={true}>Connect FitBit</Button>
+            }
           </CardContentRow>
           <CardContentRow>
             <div className="flex flex-col">
               <span>Expires on: <b>January 1, 2025</b></span>
               <span>We will no longer collect your data after this date.</span>
             </div>
+          </CardContentRow>
+        </Card>
+        <Card width="sm">
+          <CardContentRow>
+            <Title>Your Data</Title>
+          </CardContentRow>
+          <CardContentRow>
+            <div className="flex flex-col">
+              <div className="flex flex-col mb-4">
+                <span>Data being collected:</span>
+                {scope.map((s, i) =>
+                  <span key={i} className="font-bold">
+                    &nbsp;&nbsp;&nbsp;&nbsp;{s}
+                  </span>
+                )}
+              </div>
+              <div className="flex flex-col">
+                <span>Between these dates:</span>
+                <span className="font-bold">
+                  &nbsp;&nbsp;&nbsp;&nbsp;{startDate?.toLocaleDateString()} - {endDate?.toLocaleDateString()}
+                </span>
+              </div>
+            </div>
+          </CardContentRow>
+          <CardContentRow>
+            <Title>Why are we collecting your data?</Title>
+          </CardContentRow>
+          <CardContentRow>
+            <span>{dataSummary}</span>
+          </CardContentRow>
+          <CardContentRow>
+            <Title>Manage my data</Title>
+          </CardContentRow>
+          <CardContentRow>
+            <Link onClick={handleClickManageData}>
+              Request deletion of my account or data.
+            </Link>
           </CardContentRow>
         </Card>
       </ViewContainer>
