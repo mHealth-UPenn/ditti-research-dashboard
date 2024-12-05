c0d90cc8c75167f6316b31081123de709b7bc0a8
Jesse Stover <j.stover.r@gmail.com>
2024-12-05 17:32:05 -0600

    Async invoke endpoint in dev environments

--- aws_portal/config.py
+++ aws_portal/config.py
@@ -54,6 +54,9 @@ class Default:
     LAMBDA_SECRET_ACCESS_KEY = os.environ.get("LAMBDA_SECRET_ACCESS_KEY")
     LAMBDA_AWS_REGION = os.environ.get("LAMBDA_AWS_REGION", "us-east-1")
 
+    # Configuration for invoking a lambda function locally
+    LOCAL_LAMBDA_ENDPOINT = os.environ.get("LOCAL_LAMBDA_ENDPOINT")
+
     # APScheduler configuration
     # SCHEDULER_API_ENABLED = True
     # JOBS = [
