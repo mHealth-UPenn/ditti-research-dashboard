9c1caf5248f3f1169c07b41763dcd8e0aa96218e
chris01b <cbid@sas.upenn.edu>
2025-04-16 20:40:24 -0400

    Rename .prettierrc to pretter.config.js for consistency

---
+++ frontend/prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  "semi": true,
+  "trailingComma": "none",
+  "singleQuote": false,
+  "printWidth": 80
+}
+
+export default config;
