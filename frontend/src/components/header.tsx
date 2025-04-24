62150535fd69d896917342a794f31379228195bc
chris01b <cbid@sas.upenn.edu>
2025-04-24 08:25:50 +0200

    Resolve linting errors in header component
    - Remove unnecessary conditional check for accountInfo
    - Replace logical OR with nullish coalescing operator for phoneNumber

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -36,12 +36,12 @@ export const Header = () => {
 
   useEffect(() => {
     // If using researcher auth, use account info from Cognito
-    if (isResearcherAuthenticated && accountInfo) {
+    if (isResearcherAuthenticated) {
       setAccountDetails({
         firstName: accountInfo.firstName,
         lastName: accountInfo.lastName,
         email: accountInfo.email,
-        phoneNumber: accountInfo.phoneNumber || "",
+        phoneNumber: accountInfo.phoneNumber ?? "",
       });
       setLoading(false);
     }
