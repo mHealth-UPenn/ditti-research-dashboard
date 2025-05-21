0465c5e6cc0907476eac782867aa2d7d78417b8a
chris01b <cbid@sas.upenn.edu>
2025-05-21 16:47:41 -0400

    Add license to http.helpers.ts

--- frontend/src/lib/http.helpers.ts
+++ frontend/src/lib/http.helpers.ts
@@ -1,9 +1,14 @@
-/* Shared utilities for HttpClient ------------------------------------------------
+/* Copyright 2025 The Trustees of the University of Pennsylvania
  *
- * These helpers are intentionally extracted into a standalone module to avoid
- * circular-import headaches between utils.ts and the core HttpClient class.
- * They are pure functions with no dependency on the Axios instance itself so
- * they can be reused across the frontend codebase.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may]
+ * not use this file except in compliance with the License. You may obtain a
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 
 import { isCancel, isAxiosError } from "axios";
