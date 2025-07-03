8bde4b33f423b519588b00c354c59050fb5dae35
Jesse Stover <j.stover.r@gmail.com>
2025-07-03 15:54:10 -0500

    Add IAMSqlAlchemy extension for enhanced database authentication
    - Introduced IamSqlAlchemy class to extend SQLAlchemy for IAM authentication support.
    - Implemented init_app method to configure IAM authentication and SSL settings.
    - Added create_auth_token method to generate IAM authentication tokens for database connections.
    - Updated db instance to use IamSqlAlchemy for improved database connection handling.

--- functions/db_bootstrapper/backend/extensions.py
+++ functions/db_bootstrapper/backend/extensions.py
@@ -10,8 +10,77 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from typing import Any
+
+import boto3
+from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Dialect, event
+from sqlalchemy.pool import ConnectionPoolEntry
 
-db = SQLAlchemy()
+
+class IamSqlAlchemy(SQLAlchemy):
+    def init_app(
+        self,
+        app: Flask,
+        use_iam: bool = False,
+        sslmode: str = "require",
+        **kwargs,
+    ):
+        """
+        Initialize the SQLAlchemy extension.
+
+        This extends the SQLAlchemy extension to use IAM authentication when
+        use_iam is True.
+
+        Args
+        ----
+            app: The Flask application instance.
+            use_iam: Whether to use IAM authentication.
+            sslmode: The SSL mode to use.
+            **kwargs: Additional keyword arguments to pass to the SQLAlchemy
+                extension.
+        """
+        super().init_app(app, **kwargs)
+        self.client = boto3.client("rds")
+
+        with app.app_context():
+
+            @event.listens_for(self.engine, "do_connect")
+            def provide_token(
+                dialect: Dialect,  # noqa: ARG001
+                conn_rec: ConnectionPoolEntry,  # noqa: ARG001
+                cargs: tuple[Any, ...],  # noqa: ARG001
+                cparams: dict[str, Any],
+            ):
+                cparams["sslmode"] = sslmode
+
+                if use_iam:
+                    cparams["password"] = self.create_auth_token(
+                        hostname=cparams["host"],
+                        port=cparams["port"],
+                        username=cparams["user"],
+                    )
+
+    def create_auth_token(
+        self, *, hostname: str, port: int, username: str
+    ) -> str:
+        """Create an IAM authentication token for the given database URI and username."""
+        print(f"Creating IAM authentication token for user {username}")  # noqa: T201
+
+        try:
+            auth_token = self.client.generate_db_auth_token(
+                DBHostname=hostname,
+                Port=port,
+                DBUsername=username,
+            )
+        except Exception as e:
+            print(f"Error creating IAM authentication token: {e}")  # noqa: T201
+            raise
+
+        return auth_token
+
+
+db = IamSqlAlchemy()
 migrate = Migrate()
