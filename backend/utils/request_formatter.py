cf64c3f0a4df702e785b7eac235d1b1e00fb4f29
Jesse Stover <j.stover.r@gmail.com>
2025-07-05 15:31:43 -0500

    Add RequestFormatter class for enhanced logging with request context

---
+++ backend/utils/request_formatter.py
@@ -0,0 +1,15 @@
+import logging
+
+from flask import has_request_context, request
+
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord):
+        if has_request_context():
+            context = request.environ.get("lambda.context", None)
+            if context:
+                record.request_id = context.aws_request_id
+        else:
+            record.request_id = None
+
+        return super().format(record)
