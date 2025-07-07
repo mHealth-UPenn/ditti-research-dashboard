215adbbc4e90f8276a744efdc163d226c14c353f
Jesse Stover <j.stover.r@gmail.com>
2025-07-07 16:11:13 -0500

    Add configuration module for wearable data retrieval
    - Introduced a new config.py file to manage environment variables and application configuration for the wearable data retrieval functionality.
    - Implemented validation for required environment variables and defined TypedDicts for structured configuration data.
    - Added a load_config function to retrieve and construct the configuration based on environment settings.

---
+++ functions/wearable_data_retrieval/config.py
@@ -0,0 +1,78 @@
+import os
+from typing import TypedDict
+
+from shared.lambda_secrets_provider import SecretProvider
+
+LOCAL = os.getenv("LOCAL", "false") == "true"
+
+required_env_vars = [
+    "S3_BUCKET_NAME",
+    "DB_URI",
+]
+
+missing_env_vars = [var for var in required_env_vars if var not in os.environ]
+if missing_env_vars:
+    raise ValueError(
+        f"Missing required environment variables: {missing_env_vars}"
+    )
+
+
+nonlocal_required_env_vars = [
+    "FITBIT_TOKENS_SECRET_NAME",
+    "FITBIT_SECRET_NAME",
+]
+
+missing_nonlocal_env_vars = [
+    var for var in nonlocal_required_env_vars if var not in os.environ
+]
+if missing_nonlocal_env_vars and not LOCAL:
+    raise ValueError(
+        f"Missing required environment variables: {missing_nonlocal_env_vars}"
+    )
+
+
+class FitbitConfig(TypedDict):
+    client_id: str
+    client_secret: str
+
+
+class DBConfig(TypedDict):
+    uri: str
+    use_iam: bool
+
+
+class S3Config(TypedDict):
+    bucket_name: str
+
+
+class Config(TypedDict):
+    db: DBConfig
+    fitbit: FitbitConfig | None
+    s3: S3Config
+    log_level: str
+    local: bool
+    fitbit_tokens_secret_name: str | None
+
+
+def load_config() -> Config:
+    fitbit_secret = None
+    if fitbit_secret_name := os.getenv("FITBIT_SECRET_NAME"):
+        fitbit_secret = SecretProvider[FitbitConfig](
+            secret_name=fitbit_secret_name
+        ).get_secret()
+
+    config = Config(
+        db=DBConfig(
+            uri=os.getenv("DB_URI"),
+            use_iam=os.getenv("DB_USE_IAM", "false") == "true",
+        ),
+        s3=S3Config(
+            bucket_name=os.getenv("S3_BUCKET_NAME"),
+        ),
+        fitbit=fitbit_secret.secret_dict if fitbit_secret else None,
+        log_level=os.getenv("LOG_LEVEL", "INFO"),
+        local=LOCAL,
+        fitbit_tokens_secret_name=os.getenv("FITBIT_TOKENS_SECRET_NAME"),
+    )
+
+    return config
