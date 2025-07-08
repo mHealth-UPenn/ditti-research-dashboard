66ae29606518f6e8caeb013fdd58676c4f1360b8
Jesse Stover <j.stover.r@gmail.com>
2025-07-08 17:49:04 -0500

    Refactor IAM SQLAlchemy tests to utilize environment-based database URI
    - Updated `mock_app` fixture to use `FLASK_DB` environment variable for database URI configuration.
    - Introduced `db_uri_params` fixture to extract and provide database connection parameters for tests.
    - Modified test cases to assert against parameters derived from the environment, enhancing flexibility and security in test setups.

--- tests/tests_shared/test_iam_sqlalchemy.py
+++ tests/tests_shared/test_iam_sqlalchemy.py
@@ -10,13 +10,13 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import os
 from collections.abc import Callable
 from typing import Any
 from unittest.mock import MagicMock
 
 import pytest
 from flask import Flask
-from moto import mock_aws
 from sqlalchemy import Dialect, event, text
 from sqlalchemy.pool import ConnectionPoolEntry
 
@@ -26,9 +26,7 @@ from shared.iam_sqlalchemy import IamSqlAlchemy
 @pytest.fixture
 def mock_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "postgresql://username:password@localhost:5432/postgres"
-    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("FLASK_DB")
     return app
 
 
@@ -37,6 +35,21 @@ def mock_db():
     db = IamSqlAlchemy()
     db.client.generate_db_auth_token = MagicMock()
     return db
+
+
+@pytest.fixture
+def db_uri_params():
+    uri = os.getenv("FLASK_DB")
+    print(uri)
+    x = {
+        "hostname": uri.split("@")[1].split(":")[0],
+        "port": int(uri.split(":")[3].split("/")[0]),
+        "database": uri.split("/")[-1],
+        "username": uri.split("@")[0].split(":")[1][2:],
+        "password": uri.split("@")[0].split(":")[2],
+    }
+    print(x)
+    return x
 
 
 def set_connection_test(app: Flask, db: IamSqlAlchemy, test_func: Callable):
@@ -55,13 +68,18 @@ def set_connection_test(app: Flask, db: IamSqlAlchemy, test_func: Callable):
 class TestIamSqlAlchemy:
     """Test cases for the IamSqlAlchemy class."""
 
-    def test_init_app_without_iam(self, mock_app: Flask, mock_db: IamSqlAlchemy):
+    def test_init_app_without_iam(
+        self,
+        mock_app: Flask,
+        mock_db: IamSqlAlchemy,
+        db_uri_params: dict[str, Any],
+    ):
         """Test init_app when use_iam is False."""
         mock_db.init_app(mock_app, use_iam=False)
 
         def test_func(cparams: dict[str, Any]):
             assert cparams.get("sslmode") is None
-            assert cparams["password"] == "password"  # noqa: S105
+            assert cparams["password"] == db_uri_params["password"]
 
         set_connection_test(mock_app, mock_db, test_func)
 
@@ -70,16 +88,21 @@ class TestIamSqlAlchemy:
 
         mock_db.client.generate_db_auth_token.assert_not_called()
 
-    def test_init_app_with_iam(self, mock_app: Flask, mock_db: IamSqlAlchemy):
+    def test_init_app_with_iam(
+        self,
+        mock_app: Flask,
+        mock_db: IamSqlAlchemy,
+        db_uri_params: dict[str, Any],
+    ):
         """Test init_app when use_iam is True."""
         mock_db.init_app(mock_app, use_iam=True)
 
         def test_func(cparams: dict[str, Any]):
             assert cparams["sslmode"] == "require"
-            assert cparams["password"] != "password"  # noqa: S105
+            assert cparams["password"] != db_uri_params["password"]
             # Reset auth to avoid an authentication error
             del cparams["sslmode"]
-            cparams["password"] = "password"  # noqa: S105
+            cparams["password"] = db_uri_params["password"]
 
         set_connection_test(mock_app, mock_db, test_func)
 
@@ -87,11 +110,16 @@ class TestIamSqlAlchemy:
             mock_db.session.execute(text("SELECT 1"))
 
         mock_db.client.generate_db_auth_token.assert_called_once_with(
-            DBHostname="localhost", Port=5432, DBUsername="username"
+            DBHostname=db_uri_params["hostname"],
+            Port=db_uri_params["port"],
+            DBUsername=db_uri_params["username"],
         )
 
     def test_init_app_with_custom_ssl_mode(
-        self, mock_app: Flask, mock_db: IamSqlAlchemy
+        self,
+        mock_app: Flask,
+        mock_db: IamSqlAlchemy,
+        db_uri_params: dict[str, Any],
     ):
         """Test init_app with custom SSL mode."""
         mock_db.init_app(mock_app, use_iam=True, iam_sslmode="prefer")
@@ -100,16 +128,18 @@ class TestIamSqlAlchemy:
             assert cparams["sslmode"] == "prefer"
             # Reset auth to avoid an authentication error
             del cparams["sslmode"]
-            cparams["password"] = "password"  # noqa: S105
+            cparams["password"] = db_uri_params["password"]
 
         set_connection_test(mock_app, mock_db, test_func)
 
         with mock_app.app_context():
             mock_db.session.execute(text("SELECT 1"))
 
-    @mock_aws
     def test_create_auth_token_exception_propagation(
-        self, mock_app: Flask, mock_db: IamSqlAlchemy
+        self,
+        mock_app: Flask,
+        mock_db: IamSqlAlchemy,
+        db_uri_params: dict[str, Any],
     ):
         """Test that exceptions from boto3 are properly propagated."""
         mock_db.init_app(mock_app, use_iam=True)
@@ -120,7 +150,9 @@ class TestIamSqlAlchemy:
         with pytest.raises(Exception, match="AWS Error"), mock_app.app_context():
             mock_db.session.execute(text("SELECT 1"))
 
-    def test_init_app_with_additional_kwargs(self, mock_app: Flask):
+    def test_init_app_with_additional_kwargs(
+        self, mock_app: Flask, db_uri_params: dict[str, Any]
+    ):
         """Test that additional kwargs are passed to the parent init_app method."""
         db = IamSqlAlchemy(
             engine_options={"connect_args": {"dbname": "test-dbname"}}
@@ -130,7 +162,7 @@ class TestIamSqlAlchemy:
         def test_func(cparams: dict[str, Any]):
             assert cparams["dbname"] == "test-dbname"
             # Reset auth to avoid an authentication error
-            cparams["dbname"] = "postgres"
+            cparams["dbname"] = db_uri_params["database"]
 
         set_connection_test(mock_app, db, test_func)
 
